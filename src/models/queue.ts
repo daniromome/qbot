9240a260c384ee0bdac197282239bc9d700cbd65
dani9oo <dani9oo.dev@gmail.com>
2020-12-30 18:26:54 -0600

    Added queue methods for new commands

--- src/models/queue.ts
+++ src/models/queue.ts
@@ -33,7 +33,7 @@ class Queue {
         if (this.songList.length < 1) return null; return this.songList;
     }
     get startQueue() {
-        if (this.songList.length > 1) return false; return true;
+        return this.songList.length === this.nowPlaying+1
     }
     addToQueue(song: Song) {
         this.songList.push(song);
@@ -41,7 +41,7 @@ class Queue {
     skip() {
         if (this.nowPlaying + 1 >= this.songList.length && this.loop === 'disabled') throw new Error('There are no more songs in the queue');
         if (this.loop === 'one') return this.songList[this.nowPlaying];
-        if (this.nowPlaying >= this.songList.length && this.loop === 'queue') return this.songList[0];
+        if (this.nowPlaying + 1 >= this.songList.length && this.loop === 'queue') return this.songList[0];
         return this.songList[++this.nowPlaying];
     }
     play(song: Song, channel: TextChannel | DMChannel | NewsChannel) {
@@ -65,15 +65,54 @@ class Queue {
                     );
                 } catch (error) {
                     this.connection?.disconnect();
+                    this.connection = null;
                 }
             });
         } else {
             throw new Error('I am unable to play anything while disconnected from a channel')
         }
     }
+    stop() {
+        if (this.connection) {
+            this.connection.dispatcher.pause();
+        } else {
+            throw new Error('I\'m not playing anything bud');
+        }
+    }
+    resume() {
+        if (this.connection && this.connection.dispatcher.paused) {
+            this.connection.dispatcher.resume();
+        } else {
+            throw new Error('I am already playing music -_-');
+        }
+    }
     jump(number: number) {
         const position = number-1;
+        this.nowPlaying = position;
         if (number <= this.songList.length) return this.songList[position]; throw new Error(`There's no song in the position #${number}, select a song between 1 and ${this.songList.length}`);
+    }
+    kill() {
+        if (this.connection) {
+            this.connection.disconnect();
+            this.connection = null;
+            this.songList = [];
+        } else {
+            throw new Error('I am not connected to a Voice Channel!')
+        }
+    }
+    loopQueue() {
+        switch (this.loop) {
+            case 'disabled':
+                this.loop = 'queue';
+            break;
+            case 'queue':
+                this.loop = 'one';
+            break;
+            case 'one':
+                this.loop = 'disabled';
+            break;
+        }
+        return this.loop;
     }
 }
 
